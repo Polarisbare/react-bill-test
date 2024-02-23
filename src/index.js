b7d856b3de5643d6a97ad9d555077c33bb63fe04
Lv Jingxin <lv510987@163.com>
2024-02-23 14:53:18 +0800

    feat: 主题定制化

--- src/index.js
+++ src/index.js
@@ -2,17 +2,18 @@
  * @Author: Lv Jingxin lv510987@163.com
  * @Date: 2024-02-23 09:48:51
  * @LastEditors: Lv Jingxin lv510987@163.com
- * @LastEditTime: 2024-02-23 14:26:02
+ * @LastEditTime: 2024-02-23 14:38:29
  * @FilePath: /react-bill-test/src/index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
-
 import sum from "@/test";
+
+// 导入定制主题文件
+import "./theme.css";
 
 const res = sum(1, 3);
 console.log("=========>res", res);
--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,9 +1,10 @@
+import { Button } from "antd-mobile";
 import { Outlet } from "react-router-dom";
 /*
  * @Author: Lv Jingxin lv510987@163.com
  * @Date: 2024-02-23 14:17:25
  * @LastEditors: Lv Jingxin lv510987@163.com
- * @LastEditTime: 2024-02-23 14:28:29
+ * @LastEditTime: 2024-02-23 14:41:04
  * @FilePath: /react-bill-test/src/pages/Layout/index.js
  * @Description: Layout页面
  */
@@ -12,6 +13,12 @@ const Layout = () => {
     <div>
       我是layout
       <Outlet />
+      {/* 测试全局生效样式 */}
+      <Button color="primary">测试全局</Button>
+      <div className="puple">
+        {/* 测试局部生效样式 */}
+        <Button color="primary">测试局部</Button>
+      </div>
     </div>
   );
 };
