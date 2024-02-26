c8a36bc5a7c0bd6b17abfb68f8f7b0c4aaa0310a
Lv Jingxin <lv510987@163.com>
2024-02-26 11:37:14 +0800

    feat: 路由跳转TabBar实现

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -2,31 +2,66 @@
  * @Author: Lv Jingxin lv510987@163.com
  * @Date: 2024-02-23 14:17:25
  * @LastEditors: Lv Jingxin lv510987@163.com
- * @LastEditTime: 2024-02-23 16:16:15
+ * @LastEditTime: 2024-02-26 11:30:50
  * @FilePath: /react-bill-test/src/pages/Layout/index.js
  * @Description: Layout页面
  */
-import { Button } from "antd-mobile";
+import { TabBar } from "antd-mobile";
 import { useEffect } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { getBillList } from "../../store/modules/billStore";
+import { getBillList } from "@/store/modules/billStore";
+import "./index.scss";
+import {
+  BillOutline,
+  CalculatorOutline,
+  AddCircleOutline,
+} from "antd-mobile-icons";
+
+const tabs = [
+  {
+    key: "/month",
+    title: "月度账单",
+    icon: <BillOutline />,
+  },
+  {
+    key: "/new",
+    title: "记账",
+    icon: <AddCircleOutline />,
+  },
+  {
+    key: "/year",
+    title: "年度账单",
+    icon: <CalculatorOutline />,
+  },
+];
+
 const Layout = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getBillList());
   }, [dispatch]);
+
+  // 切换菜单跳转路由
+  const navigate = useNavigate();
+  const swithRoute = (path) => {
+    console.log(path);
+    navigate(path);
+  };
   return (
-    <div>
-      我是layout
-      <Outlet />
-      {/* 测试全局生效样式 */}
-      <Button color="primary">测试全局</Button>
-      <div className="puple">
-        {/* 测试局部生效样式 */}
-        <Button color="primary">测试局部</Button>
+    <div className="layout">
+      <div className="container">
+        <Outlet />
+      </div>
+      <div className="footer">
+        <TabBar onChange={swithRoute}>
+          {tabs.map((item) => (
+            <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+          ))}
+        </TabBar>
       </div>
     </div>
   );
 };
+
 export default Layout;
